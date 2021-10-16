71c8b521c65e15d8af0d9fb6f9cdad5013acb65f
Lucas Kent <rubickent@gmail.com>
2021-10-16 18:54:00 +1100

    Add TODO explaining findings

--- src/renderer/app/mod.rs
+++ src/renderer/app/mod.rs
@@ -43,13 +43,46 @@ pub async fn render_window_wasm(subaction: HighLevelSubaction) {
 
     let document = web_sys::window().unwrap().document().unwrap();
 
-    let visualiser_span = document.get_element_by_id("visualiser").unwrap();
+    let visualiser_span = document.get_element_by_id("fighter-render").unwrap();
     visualiser_span
         .append_child(&web_sys::Element::from(window.canvas()))
         .unwrap();
 
     let mut app = App::new(window, subaction).await;
     let event_tx = app.get_event_tx();
+
+    // TODO:
+    // hmmmmmmmmmmmmmmmmmm
+    // It would be ideal if we could completely control the app from here by just sending events.
+    // Two way communication would add a lot of complexity.
+    // The tricky part is how to handle updating the current frame.
+    // Can we do that without access to the render loop?
+
+    // possible approaches:
+    // ## directly use surface?
+    // * yew
+    // * how do I get a surface for wgpu
+    //
+    // ## winit + reuse app + move rukaidata UI into brawllib via iced
+    // * send state in with event
+    // * AppState just stores state and everyone is responsible for sending in AppEvent to update it.
+    //
+    // ## winit + reuse App/AppState
+    // * send state in with event
+    // * AppState just stores state and everyone is responsible for sending in AppEvent to update it.
+    //
+    // ## winit + dont reuse App/AppState
+    // * reimplement equivalent of App for maximum flexibility
+    //
+    // wow I really cant figure out which one I want.
+    // Im thinking:
+    // 1. get this running in rukaidata so I can test things properly
+    // 2. go with: "winit + dont reuse app", then prototype really quickly to see if it works.
+    // 3. move on with my life. I really dont think I care about having to focus the app to give keyboard inputs.
+
+    let frame = document.get_element_by_id("frame").unwrap();
+    let frame_move = frame.clone();
+    frame_move.set_inner_html("Frame: 0");
 
     let button = document.get_element_by_id("run").unwrap();
     let button_move = button.clone();
